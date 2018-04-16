b103284f6503036491d945992163a11f63388bc6
Ivelisse Cintron <33500791+ivelissecintron@users.noreply.github.com>
2018-04-16 13:08:04 -0500

    updated html/css/js/

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,12 @@
+// start timer starts the counter
+// time remaining 120 seconds
+// when the timer is done alert all done and documents correct & incorrect & unanswered
+// done button
 //* You'll create a trivia form with multiple choice or true/false options (your choice).
 //* The player will have a limited amount of time to finish the quiz. 
 //* The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
 //* Don't let the player pick more than one answer per question.
 //* Don't forget to include a countdown timer.
 //* Get music to play at the end of the game
+
+//correct answers D, A, B, A, C
